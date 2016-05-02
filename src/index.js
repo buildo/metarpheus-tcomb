11a67ab5268d4061f7a45b23f5c288b86440c44c
Giovanni Gonzaga <giovanni@buildo.io>
2016-05-02 21:49:47 +0200

    require from cwd

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import metarpheusTcomb from './metarpheusTcomb';
 import IntermRep from './IntermRep';
 import Config from './Config';
 import fs from 'fs';
+import path from 'path';
 
 const argv = yargs.argv;
 const configFileArg = 'config';
@@ -32,9 +33,9 @@ const {
   apiOut = '',
   overrides = {},
   renameModel = v => v
-} = Config(require(argv[configFileArg]));
+} = Config(require(path.resolve(process.cwd(), argv[configFileArg])));
 
-const intermRep = IntermRep(require(intermRepIn));
+const intermRep = IntermRep(require(path.resolve(process.cwd(), intermRepIn)));
 
 const { model, api } = metarpheusTcomb({ intermRep, overrides, modelPrelude, apiPrelude, apiModelPrefix, renameModel });
 
