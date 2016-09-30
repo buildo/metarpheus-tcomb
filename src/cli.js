0d64b5be93e835380370e4d748ae4cd5bb1317d8
Giovanni Gonzaga <giovanni@buildo.io>
2016-09-30 18:25:28 +0200

    split index in cli.js and index.js (for node)

---
+++ src/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs';
+import metarpheusTcomb from './index';
+import fs from 'fs';
+import path from 'path';
+import CliConfig from './CliConfig';
+
+const argv = yargs.argv;
+const configFileArg = 'config';
+if (!argv[configFileArg]) {
+  throw new Error(`missing --${configFileArg}=path/to/config.js[on]`);
+}
+
+require('babel/register')({
+  only: new RegExp(argv[configFileArg].replace(/\.js$/, '')), // so that config file can be ESwhatever
+  extensions: ['.js'],
+  stage: 0,
+  loose: true
+});
+
+const config = CliConfig(require(path.resolve(process.cwd(), argv[configFileArg])));
+const intermRep = require(path.resolve(process.cwd(), config.intermRepIn));
+
+const { model, api } = metarpheusTcomb({ intermRep, config });
+
+fs.writeFileSync(config.modelOut, model);
+fs.writeFileSync(config.apiOut, api);
